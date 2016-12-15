56fca9c9028ec9ac43d91ee48cab59971104b44a
ntemposd <ntemposd@gmail.com>
2016-12-15 18:06:53 +0200

    BETA Launch
    Fixed meta, indentation and backslashes. Also erased comments and
    cleaned code to make it readable

--- app.php
+++ app.php
@@ -1,5 +1,5 @@
 <?php
-if (isset($_POST["submit"])) {
+ if (isset($_POST["submit"])) {
       $name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
       $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
       $phone = htmlspecialchars($_POST['phone'], ENT_QUOTES, 'UTF-8');
@@ -13,38 +13,30 @@ if (isset($_POST["submit"])) {
       $subject = '=?UTF-8?B?' . base64_encode('Νέα αίτηση συμμετοχής') . '?=';
 
       $body = "From: $name\n E-Mail: $email\n Answer-1:\n $message1 Answer-2:\n $message2 Answer-3:\n $message3 Answer-4:\n $message4";
-      // Check if name has been entered
       if (!$_POST['name']) {
           $errName = 'Συμπλήρωσε το όνομα σου';
       }
-      // Check if email has been entered and is valid
       if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
           $errEmail = 'Η διεύθυνση email δεν είναι υπαρκτή';
       }
       if (!$_POST['phone']) {
           $errPhone = 'Συμπλήρωσε το όνομα σου';
       }
-      // Check if message has been entered and is valid
       if (!$_POST['message1']) {
           $errMessage1 = 'Υποχρεώτικό πεδίο';
       }
-      // Check if message has been entered and is valid
       if (!$_POST['message2']) {
           $errMessage2 = 'Υποχρεώτικό πεδίο';
       }
-      // Check if message has been entered and is valid
       if (!$_POST['message3']) {
           $errMessage3 = 'Υποχρεώτικό πεδίο';
       }
-      // Check if message has been entered and is valid
       if (!$_POST['message4']) {
           $errMessage4 = 'Υποχρεώτικό πεδίο';
       }
-      //Check if simple anti-bot test is correct
       if ($human !== 5) {
           $errHuman = 'Η απάντηση δεν είναι σωστή';
       }
-     // If there are no errors, send the email
       if (!$errName && !$errEmail && !$errMessage1 && !$errHuman) {
           if (mail($to, $subject, $body, $from)) {
             $result = '<div class="alert alert-success">Το email σου έχει ληφθεί! Θα ενημερωθείς για την πορεία της αίτησης σου με την ολοκλήρωση της διαδικασίας αξιολόγησης.</div>';
@@ -61,27 +53,32 @@ if (isset($_POST["submit"])) {
   <meta http-equiv="encoding" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>OK!Thess - Αίτηση Συμμετοχής</title>
+  <title>OK!Thess | Αίτηση Συμμετοχής</title>
   <meta http-equiv="Content-Type" content="text/html">
   <meta name="description" content="Γίνονται δεκτές καινοτόμες προτάσεις από άτομα ή ομάδες, ανεξάρτητα του βαθμού ωρίμανσης, που θα ήθελαν να ωφεληθούν από τις υπηρεσίες της προ-θερμοκοιτίδας του OK!Thess.">
   <meta name="robots" content="NOODP">
   <meta name="author" content="ntemposd">
 
-  <!-- Schema.org markup for Google+ -->
   <meta itemprop="name" content="OK!Thess">
-  <meta itemprop="description" content="Οικοσύστημα Καινοτομίας Θεσσαλονίκης">
-  <meta itemprop="image" content="img/logo.png">
+  <meta itemprop="description" content="Γίνονται δεκτές καινοτόμες προτάσεις από άτομα ή ομάδες, ανεξάρτητα του βαθμού ωρίμανσης, που θα ήθελαν να ωφεληθούν από τις υπηρεσίες της προ-θερμοκοιτίδας του OK!Thess.">
+  <meta itemprop="image" content="/img/thumb.png">
 
-  <!-- Open Graph data -->
+  <meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@okthess">
+	<meta name="twitter:creator" content="@ntemposd">
+	<meta name="twitter:title" content="OK!Thess">
+	<meta name="twitter:description" content="Γίνονται δεκτές καινοτόμες προτάσεις από άτομα ή ομάδες, ανεξάρτητα του βαθμού ωρίμανσης, που θα ήθελαν να ωφεληθούν από τις υπηρεσίες της προ-θερμοκοιτίδας του OK!Thess.">
+	<meta name="twitter:image" content="http://www.okthess.gr/img/thumb.png">
+
   <meta property="og:title" content="OK!Thess">
-  <meta property="og:description" content="Οικοσύστημα Καινοτομίας Θεσσαλονίκης">
+  <meta property="og:description" content="Γίνονται δεκτές καινοτόμες προτάσεις από άτομα ή ομάδες, ανεξάρτητα του βαθμού ωρίμανσης, που θα ήθελαν να ωφεληθούν από τις υπηρεσίες της προ-θερμοκοιτίδας του OK!Thess.">
   <meta property="og:url" content="http://okthess.gr">
-  <meta property="og:image" content="img/logo.png">
+  <meta property="og:image" content="/img/thumb.png">
   <meta property="og:type" content="website">
 
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="style/main.css">
-  <link rel="stylesheet" type="text/css" href="style/app.css">
+  <link rel="stylesheet" type="text/css" href="/style/main.css">
+  <link rel="stylesheet" type="text/css" href="/style/app.css">
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700&amp;subset=greek,greek-ext" rel="stylesheet">
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -96,9 +93,9 @@ if (isset($_POST["submit"])) {
 <nav class="navbar navbar-default container-fluid">
   <div class="navbar-header">
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
+     <span class="icon-bar"></span>
+     <span class="icon-bar"></span>
+     <span class="icon-bar"></span>
     </button>
     <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="Logo"></a>
   </div>
@@ -132,12 +129,11 @@ if (isset($_POST["submit"])) {
   <p>Οι προτάσεις αξιολογούνται σε τακτά χρονικά διαστήματα, οι ακριβείς ημερομηνίες ανακοινώνονται στην παρούσα ιστοσελίδα. Η τελευταία περίοδος αξιολόγησης έκλεισε την 1η Δεκέμβρη 2016 και η επόμενη έχει προγραμματιστεί για τις αρχές του 2017. Η ακριβής ημερομηνία θα ανακοινωθεί στο αμέσως προσεχές μέλλον.</p>
   <p>Για συμμετοχή συμπληρώστε την παρακάτω φόρμα συμμετοχής ή απαντήστε τις ερωτήσεις στο σώμα ενώς e-mail και στείλτε στο <strong><small>apply@okthess.gr</small></strong></p>
   <br>
-  <a class="btn btn-primary" href="http://okthess.gr/pdf/website-open-call.pdf" role="button">Πληροφορίες</a>
-  <a class="btn btn-primary" href="http://okthess.gr/under-construction.html" role="button">Eσωτερικός κανονισμός</a>
+  <a class="btn btn-primary" href="http://okthess.gr/pdf/website-open-call.pdf" role="button" target="_blank">Πληροφορίες</a>
+  <a class="btn btn-primary" href="http://okthess.gr/under-construction.html" role="button" target="_blank">Eσωτερικός κανονισμός</a>
   <br>
   <br>
   <h2 class="text-muted">Φόρμα Συμμετοχής</h2>
-  <p><small>(Η φόρμα δεν λειτουργεί προσωρινά για τεχνικούς λόγους)</small></p>
   ---------------
   <form class="form-horizontal" role="form" method="post" action="app.php">
     <div class="form-group">
@@ -201,43 +197,43 @@ if (isset($_POST["submit"])) {
   </form>
 </div>
 <footer class="site-footer">
- <div class="container">
-	<div class="row" id="social">
-	 <a href="mailto:mail@okthess.gr" target="_blank"><img social src="img/email.png" alt="email"></a>
-	 <a href="https://www.facebook.com/OKThess/" target="_blank"><img social src="img/facebook.png" alt="facebook"></a>
-	 <a href="https://twitter.com/okthess" target="_blank"><img social src="img/twitter.png" alt="twitter"></a>
-	</div>
-	<div class="row">
-		<div class="col-md-4 col-sm-4">
-			<h5>ΤΟΠΟΘΕΣΙΑ</h5>
-			<hr>
-			<div id="map-outer">
-			<div id="map-container"></div>
-				<script src="js/map.js"></script>
+	<div class="container">
+		<div class="row" id="social">
+			<a href="mailto:mail@okthess.gr" target="_blank"><img social src="/img/email.png" alt="email"></a>
+			<a href="https://www.facebook.com/OKThess/" target="_blank"><img social src="/img/facebook.png" alt="facebook"></a>
+			<a href="https://twitter.com/okthess" target="_blank"><img social src="/img/twitter.png" alt="twitter"></a>
+		</div>
+		<div class="row">
+			<div class="col-md-4 col-sm-4">
+				<h5>ΤΟΠΟΘΕΣΙΑ</h5>
+				<hr>
+					<div id="map-outer">
+						<div id="map-container"></div>
+						<script src="/js/map.js"></script>
+					</div>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<h5>ΣΥΝΕΡΓΑΤΕΣ</h5><hr>
+				<a href="https://www.auth.gr/" target="_blank"><img src="/img/AUTH.jpg" alt="Aristotle_University_Thessaloniki"></a>
+				<a href="http://www.thessaloniki.gr/" target="_blank"><img src="/img/municipality.jpg" alt="Municipality_of_Thessaloniki"></a>
+				<a href="http://www.uom.gr/" target="_blank"><img src="/img/UOM.png" alt="University_Of_Macedonia"></a>
+				<a href="http://www.thessinnozone.gr/" target="_blank"><img src="/img/TIZ.jpg" alt="Thessaloniki_Innovation_Zone"></a>
+				<a href="http://www.teithe.gr/" target="_blank"><img src="/img/ATEITH.jpg" alt="Alexander_Technological_Institute_Thessaloniki"></a>
+				<a href="http://www.sbbe.gr/" target="_blank"><img src="/img/SBBE.jpg" alt="SBBE"></a>
+				<a href="http://www.ihu.edu.gr/" target="_blank"><img src="/img/IHU.jpg" alt="International_Hellenic_University"></a>
+				<a href="http://www.seve.gr/" target="_blank"><img src="/img/SEVE.jpg" alt="SEVE"></a>
+			</div>
+			<div class="col-md-4 col-sm-4">
+				<h5>ΥΠΟΣΤΗΡΙΚΤΕΣ</h5><hr>
+				<a href="http://www.hcn.gr/" target="_blank"><img src="/img/hcn.jpg" alt="HCN"></a>
+				<a href="http://www.beetroot.gr/" target="_blank"><img src="/img/beetroot.png" alt="Beetroot"></a>
+				<a href="https://www.ast.gr/" target="_blank"><img src="/img/ast.png" alt="AST"></a>
+				<h5>ΔΩΡΗΤΗΣ</h5><hr>
+				<a href="http://www.snf.org/el/" target="_blank"><img src="/img/ISN.jpg" alt="ISN"></a>
 			</div>
 		</div>
-		<div class="col-md-4 col-sm-4">
-		<h5>ΣΥΝΕΡΓΑΤΕΣ</h5><hr>
-			<a href="https://www.auth.gr/" target="_blank"><img src="img/AUTH.jpg" alt="Aristotle_University_Thessaloniki"></a>
-			<a href="http://www.thessaloniki.gr/" target="_blank"><img src="img/municipality.jpg" alt="Municipality_of_Thessaloniki"></a>
-			<a href="http://www.uom.gr/" target="_blank"><img src="img/UOM.png" alt="University_Of_Macedonia"></a>
-			<a href="http://www.thessinnozone.gr/" target="_blank"><img src="img/TIZ.jpg" alt="Thessaloniki_Innovation_Zone"></a>
-			<a href="http://www.teithe.gr/" target="_blank"><img src="img/ATEITH.jpg" alt="Alexander_Technological_Institute_Thessaloniki"></a>
-			<a href="http://www.sbbe.gr/" target="_blank"><img src="img/SBBE.jpg" alt="SBBE"></a>
-			<a href="http://www.ihu.edu.gr/" target="_blank"><img src="img/IHU.jpg" alt="International_Hellenic_University"></a>
-			<a href="http://www.seve.gr/" target="_blank"><img src="img/SEVE.jpg" alt="SEVE"></a>
-		</div>
-		<div class="col-md-4 col-sm-4">
-		<h5>ΥΠΟΣΤΗΡΙΚΤΕΣ</h5><hr>
-			<a href="http://www.hcn.gr/" target="_blank"><img src="img/hcn.jpg" alt="HCN"></a>
-			<a href="http://www.beetroot.gr/" target="_blank"><img src="img/beetroot.png" alt="Beetroot"></a>
-			<a href="https://www.ast.gr/" target="_blank"><img src="img/ast.png" alt="AST"></a>
-		<h5>ΔΩΡΗΤΗΣ</h5><hr>
-			<a href="http://www.snf.org/el/" target="_blank"><img src="img/ISN.jpg" alt="ISN"></a>
-		</div>
-	 </div>
- </div>
-<p id="copy">Lovingly crafted by <a href="http://ntemposd.me" target="_blank">ntemposd</a></p>
+	</div>
+		<p id="copy">Lovingly crafted by <a href="http://ntemposd.me" target="_blank">ntemposd</a></p>
 </footer>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB7E2raajPS5-4vEpx-LBOV0XkItvWQRt8&signed_in=true&callback=initMap"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
