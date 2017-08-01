c4b23d0204185990ed4a6ee398b136d072b45878
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-01 20:22:57 +0300

    add teams view method

--- main/views.py
+++ main/views.py
@@ -8,3 +8,8 @@ def get_about(request):
     return render(request, 'main/about.html', {
         'page_title': 'Σχετικά',
     })
+
+def get_teams(request):
+    return render(request, 'main/teams.html', {
+        'page_title': 'Φιλοξενούμενες Ομάδες',
+    })
