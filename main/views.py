b74b641b6d7e8f21f9a4286ce893816dbaf44ddc
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-01 19:47:19 +0300

    add get about view method

--- main/views.py
+++ main/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def get_index(request):
     return render(request, 'main/index.html')
+
+def get_about(request):
+    return render(request, 'main/about.html', {
+        'page_title': 'Σχετικά',
+    })
