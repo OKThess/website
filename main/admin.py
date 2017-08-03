692b8249d038837aedf3e3ecaf9d54bf15ec4e5a
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-03 16:58:29 +0300

    register new models on admin

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Team, Job
+from .models import Team, Job, Mentor, Meetup, Coworking
 
 admin.site.register(Team)
 admin.site.register(Job)
+admin.site.register(Mentor)
+admin.site.register(Meetup)
+admin.site.register(Coworking)
