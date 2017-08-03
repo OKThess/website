88f4dbc9c64decdf07561f864f53e505f08df645
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-03 15:08:41 +0300

    update tests for new teams views

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from .models import Team, Job
+
 
 class GetViewsTest(TestCase):
     def test_index(self):
@@ -22,11 +24,44 @@ class GetViewsTest(TestCase):
         self.assertContains(response, 'OK!Thess')
         self.assertContains(response, 'Σχετικά')
 
+
+class TeamsViewTests(TestCase):
     def test_teams(self):
         """
         The get_teams view.
         """
+        new_team = Team.objects.create(
+            name = 'OK!Thess',
+            description = 'The best team.',
+            url = 'http://www.okthess.gr/',
+            image_url = 'http://www.okthess.gr/logo.png',
+        )
         url = reverse('main:teams')
         response = self.client.get(url)
         self.assertContains(response, 'OK!Thess')
         self.assertContains(response, 'Φιλοξενούμενες ομάδες')
+        self.assertContains(response, new_team.name)
+        self.assertContains(response, new_team.description)
+        self.assertContains(response, new_team.url)
+
+    def test_jobs(self):
+        """
+        The get_teams view.
+        """
+        new_team = Team.objects.create(
+            name = 'OK!Thess',
+            description = 'The best team.',
+            url = 'http://www.okthess.gr/',
+            image_url = 'http://www.okthess.gr/logo.png',
+        )
+        new_job = Job.objects.create(
+            team = new_team,
+            title = 'Web Developer',
+            apply_url = 'http://www.okthess.gr/jobs',
+        )
+        url = reverse('main:teams')
+        response = self.client.get(url)
+        self.assertContains(response, 'OK!Thess')
+        self.assertContains(response, 'Φιλοξενούμενες ομάδες')
+        self.assertContains(response, new_job.title)
+        self.assertContains(response, new_job.apply_url)
