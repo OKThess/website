d631e38e47dc668609c193fe37d06cf2bf87538e
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-03 14:47:56 +0300

    add team and job models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField()
+    image_url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    title = models.CharField(max_length=100)
+    apply_url = models.URLField()
+
+    def __str__(self):
+        return self.title
