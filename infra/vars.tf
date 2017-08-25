d652927dc7bf56d6d897cea7d4bda3fd6fd4e47a
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-25 16:41:45 +0300

    overhaul variables tf file

---
+++ infra/vars.tf
@@ -0,0 +1,15 @@
+variable "profile" {
+  description = "Name of your profile inside ~/.aws/credentials"
+}
+
+variable "application_name" {
+  description = "Web application name"
+}
+
+variable "application_description" {
+  description = "Description of Elastic Beanstalk application"
+}
+
+variable "region" {
+  description = "Defines at which AWS region your app will be deployed"
+}
