d62d8bde0d71aa3c76d3b6ab7c20cb023988d939
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-25 18:16:49 +0300

    fmt tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -6,6 +6,6 @@ resource "aws_iam_instance_profile" "okthess_beanstalk_ec2" {
 
 # Beanstalk IAM role
 resource "aws_iam_role" "okthess_beanstalk_ec2" {
-  name = "okthess-beanstalk-ec2-role"
+  name               = "okthess-beanstalk-ec2-role"
   assume_role_policy = "${file("aws_iam_role_policies/okthess-beanstalk-ec2-policy.json")}"
 }
