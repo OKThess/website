f46dcf48b1a228d1fac3fba06dedc1157b94b0c4
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-28 20:20:22 +0300

    add debug logs on settings helpers

--- okthess/helpers.py
+++ okthess/helpers.py
@@ -6,10 +6,13 @@ def is_ec2_linux():
     Detect if we are running on an EC2 Linux Instance
     See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
     """
-    if os.path.isfile("/sys/hypervisor/uuid"):
-        with open("/sys/hypervisor/uuid") as f:
+    system_uuid_filename = '/sys/hypervisor/uuid'
+    if os.path.isfile(system_uuid_filename):
+        with open(system_uuid_filename) as f:
             uuid = f.read()
-            return uuid.startswith("ec2")
+            print('is_ec2_linux = true')
+            return uuid.startswith('ec2')
+    print('is_ec2_linux = false')
     return False
 
 def get_linux_ec2_private_ip():
@@ -18,14 +21,21 @@ def get_linux_ec2_private_ip():
     See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html#instancedata-data-retrieval
     """
     if not is_ec2_linux():
+        print('this is not ec2 linux')
         return None
 
+    print('this is indeed ec2 linux')
     response = None
     try:
         response = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
-        return response.read()
+        contents = response.read()
+        print('req contents:', contents)
+        return contents
     except:
+        print('req exception')
         return None
     finally:
+        print('req finally')
         if response:
+            print('close response')
             response.close()
