d4e391e5c0fba46b85a1734a69568a4c82000885
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-08-28 19:27:34 +0300

    add dynamic private ip loading on allowed hosts

--- okthess/settings.py
+++ okthess/settings.py
@@ -31,6 +31,40 @@ ALLOWED_HOSTS = ['
     'okthess.absk3drrdz.eu-central-1.elasticbeanstalk.com',
 ']
 
+def is_ec2_linux():
+    """
+    Detect if we are running on an EC2 Linux Instance
+    See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+def get_linux_ec2_private_ip():
+    """
+    Get the private IP Address of the machine if running on an EC2 linux server
+    See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html#instancedata-data-retrieval
+    """
+    import urllib2
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urllib2.urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        return response.read()
+    except:
+        return None
+    finally:
+        if response:
+    response.close()
+
+# ElasticBeanstalk healthcheck sends requests with host header = internal ip
+# So we detect if we are in elastic beanstalk, and add the instances private ip address
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
+
 
 # Application definition
 
