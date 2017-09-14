c2c905d3339c9f3d4c339c067b396b77debbb7b9
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-14 17:24:15 +0300

    add alumni checkbox on team model

--- main/models.py
+++ main/models.py
@@ -11,6 +11,7 @@ class Team(models.Model):
         default='main/static/main/logo.png',
         max_length=250,
     )
+    alumni = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
