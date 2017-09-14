4e2efc30427b07180413fda3dd3dffb1f734622b
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-14 16:02:18 +0300

    remove job model

--- main/models.py
+++ main/models.py
@@ -12,19 +12,6 @@ class Team(models.Model):
         return self.name
 
 
-class Job(models.Model):
-    team = models.ForeignKey(
-        Team,
-        on_delete=models.SET_NULL,
-        null=True,
-    )
-    title = models.CharField(max_length=100)
-    apply_url = models.URLField()
-
-    def __str__(self):
-        return self.title
-
-
 class Mentor(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
