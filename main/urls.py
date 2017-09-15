22089b52ae9fe65139682cc448ff11eae43b99d3
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-15 21:45:14 +0300

    add media static urls on urls for dev

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from . import views
@@ -36,4 +38,5 @@ urlpatterns = [
 
     # /health
     url(r'^health/$', views.health, name='health'),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
