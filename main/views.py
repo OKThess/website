04a5265da96039a614e8a8dc2aa9d2de97343661
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-20 17:45:09 +0300

    fix events on index view method

--- main/views.py
+++ main/views.py
@@ -9,10 +9,10 @@ from .forms import ApplicationForm
 
 def index(request):
     featured_posts = Post.objects.filter(is_featured=True)
-    events = Event.objects.all()
+    events_list = Event.objects.order_by('-date')[:3]
     return render(request, 'main/index.html', {
         'featured_posts': featured_posts,
-        'events': events,
+        'events_list': events_list,
     })
 
 
