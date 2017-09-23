41d2bbb032c89f32ed752852523708cda543879b
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-23 20:38:18 +0300

    fix iam roles on tf file

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,11 +1,11 @@
-# Beanstalk IAM instance profile
-resource "aws_iam_instance_profile" "okthess_beanstalk_ec2" {
-  name = "okthess-beanstalk-ec2-user"
-  role = "${aws_iam_role.okthess_beanstalk_ec2.name}"
-}
-
 # Beanstalk IAM role
 resource "aws_iam_role" "okthess_beanstalk_ec2" {
   name               = "okthess-beanstalk-ec2-role"
   assume_role_policy = "${file("aws_iam_role_policies/okthess-beanstalk-ec2-policy.json")}"
+}
+
+# Beanstalk IAM instance profile
+resource "aws_iam_instance_profile" "okthess_beanstalk_ec2" {
+  name = "okthess-beanstalk-ec2-role"
+  role = "${aws_iam_role.okthess_beanstalk_ec2.name}"
 }
