35721b573b9ab6916bc935454ded9dc7323361f1
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-25 19:53:29 +0300

    remove content urls from main urls file

--- main/urls.py
+++ main/urls.py
@@ -1,43 +1,18 @@
 from django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 
 from . import views
 
 
-app_name = 'main'
+def health(request):
+    return HttpResponse('Ok')
+
+app_name = 'mainapp'
 admin.site.site_header = 'OK!Thess administration'
 urlpatterns = [
-    # /
-    url(r'^$', views.index, name='index'),
-
-    # /about/
-    url(r'^about/$', views.about, name='about'),
-
-    # /program/
-    url(r'^program/$', views.program_redir, name='program_redir'),
-    url(r'^program/teams/$', views.program_teams, name='program_teams'),
-    url(r'^program/mentors/$', views.program_mentors, name='program_mentors'),
-    url(r'^program/alumni/$', views.program_alumni, name='program_alumni'),
-    url(r'^program/meetup/$', views.program_meetup, name='program_meetup'),
-
-    # /events/
-    url(r'^events/$', views.events, name='events'),
-
-    # /blog/
-    url(r'^blog/$', views.blog, name='blog'),
-
-    # e.g. /blog/sample-post
-    url(r'^blog/(?P<post_slug>[^/]*)/$', views.blog_post, name='blog_post'),
-
-    # e.g. /blog/archives/2016/3
-    url(r'^blog/archives/(?P<archive_year>[^/]*)/(?P<archive_month>[^/]*)/$', views.blog_archives, name='blog_archives'),
-
-    # /contact/
-    url(r'^contact/$', views.contact, name='contact'),
-
-    # /apply/
-    url(r'^apply/$', views.apply, name='apply'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # /health
+    url(r'^health/$', health, name='health'),
+]
