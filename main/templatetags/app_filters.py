8f1d5415a48d57b07f99380b168de4fc45a6f85c
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-27 21:22:20 +0300

    add navbar active app filter

--- main/templatetags/app_filters.py
+++ main/templatetags/app_filters.py
@@ -1,5 +1,14 @@
+import re
+
 from django import template
-from django.template.defaultfilters import stringfilter
+from django.core.urlresolvers import reverse, NoReverseMatch
 
 register = template.Library()
 
+
+@register.simple_tag(takes_context=True)
+def active(context, pattern):
+    path = context['request'].path
+    if re.search(pattern, path):
+        return 'active'
+    return ''
