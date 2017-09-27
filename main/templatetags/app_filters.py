05b234c0a9fcb54aafc6a8a2e3f10913006463dd
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-27 21:21:53 +0300

    delete this useless thing

--- main/templatetags/app_filters.py
+++ main/templatetags/app_filters.py
@@ -3,13 +3,3 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
-@register.filter(name='prepend_media')
-@stringfilter
-def prepend_media(value):
-    """
-    Prepends value with static dir if not external url.
-    """
-    if value[:4] != 'http':
-        return '/uploads/' + value
-    else:
-        return value
