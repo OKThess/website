1be8efac376c3db842b79009b2a4f2cb2fe24869
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-09-29 02:14:32 +0300

    remove unused imports on content urls

--- main/content_urls.py
+++ main/content_urls.py
@@ -1,10 +1,6 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
-from django.contrib import admin
 
 from . import views
-
 
 app_name = 'main'
 urlpatterns = [
