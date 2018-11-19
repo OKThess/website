0d35b21f949fd38509f2405dc15bb72b4a54b4e0
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-11-19 19:05:04 +0200

    add debug staging commands

--- staging.sh
+++ staging.sh
@@ -1,5 +1,11 @@
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
+echo "GIT BRANCH"
+git rev-parse --abbrev-ref HEAD
+
+echo "GIT STATUS"
+git status
+
 if [ $BRANCH == "staging" ]; then
     echo "Deployment for staging starting..."
     ssh-keyscan -H staging.okthess.gr >> ~/.ssh/known_hosts
