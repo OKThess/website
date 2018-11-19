0fe533f15707a9085f6b0f260d938e56332e40c9
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-11-19 19:16:26 +0200

    try to fix ssh keyscan

--- staging.sh
+++ staging.sh
@@ -2,7 +2,7 @@
 
 if [ $TRAVIS_BRANCH == "staging" ]; then
     echo "Deployment for staging starting..."
-    ssh-keyscan -H staging.okthess.gr >> ~/.ssh/known_hosts
+    ssh-keyscan -H 138.68.74.50 >> ~/.ssh/known_hosts
     git push dokku@138.68.74.50:staging staging
 else
     echo "Not on staging, deployment canceled."
