9704b86fc890f80e7dc0e97076b25cac31741051
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-11-19 18:46:58 +0200

    add auto deployment for staging

---
+++ staging.sh
@@ -0,0 +1,9 @@
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+if [ $BRANCH == "staging" ]; then
+    echo "Deployment for staging starting..."
+    ssh-keyscan -H staging.okthess.gr >> ~/.ssh/known_hosts
+    git push dokku@138.68.74.50:staging staging
+else
+    echo "Not on staging, deployment canceled."
+fi
